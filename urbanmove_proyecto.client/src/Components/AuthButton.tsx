1ee9cab91954ad70a4f8c4f7fe2928c337175f8e
Huaynate Achachau José Luis <devlegendjs@gmail.com>
2026-07-15 22:59:52 -0500

    pense que se usaba ese evento y no

--- urbanmove_proyecto.client/src/Components/AuthButton.tsx
+++ urbanmove_proyecto.client/src/Components/AuthButton.tsx
@@ -7,12 +7,11 @@ interface AuthButtonProps {
     onClick?: () => void;
 }
 
-function AuthButton({ children, disabled, type = 'submit', onClick }: AuthButtonProps) {
+function AuthButton({ children, disabled, type = 'submit' }: AuthButtonProps) {
     return (
         <button
             type={type}
             disabled={disabled}
-            onClick={onClick}
             className="w-full bg-black py-2 text-sm font-semibold
                        text-white disabled:cursor-not-allowed disabled:opacity-50"
         >
