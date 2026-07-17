653a1bbec63970b8e295d6aab8fb3fcc2f4bc4e9
cybernetjs <devlegendjs@gmail.com>
2026-07-17 11:06:10 -0500

    creacion de DTO para registrar ciudadano

--- URBANMOVE_Proyecto.Server/Models/DTO/AuthDTO.cs
+++ URBANMOVE_Proyecto.Server/Models/DTO/AuthDTO.cs
@@ -18,4 +18,15 @@
     {
         public required string Message { get; set; }
     }
+
+    public sealed class RegisterRequest{
+
+        public required string Name {get; set;}
+        public required string LastName {get; set;}
+        public required string Email {get; set;}
+        public required string Password {get; set;}
+        public required string ConfirmPassword {get; set;}
+        
+    }
+
 }
